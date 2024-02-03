62f141654f0ffe212d6657f09c798bc479669cba
Ayush Sabharwal <ayushkumar9416427162@gmail.com>
2024-02-03 14:19:17 +0530

    project setup

---
+++ app/src/main/java/com/example/notes/LoginFragment.kt
@@ -0,0 +1,15 @@
+package com.example.notes
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LoginFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+}
